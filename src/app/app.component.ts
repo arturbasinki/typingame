924d85bcd7b75616cfa11745c212ff6bdbb12f17
airbass <artur.basinski@gmail.com>
2022-01-08 00:09:25 +0100

    Ready version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typingame';
+  randomText = 'Neque porro quisquam est qui dolorem ipsum';
+  enteredText = '';
+  onInput(event: any) {
+    this.enteredText = event.target.value;
+    // console.log(this.enteredText);
+  }
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
